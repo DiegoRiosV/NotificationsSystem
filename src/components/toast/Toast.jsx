e69938cb302f625539508b9067a7691461f07c52
DiegoRiosV <diego.rios.v@ucb.edu.bo>
2026-07-10 12:57:41 -0400

    refactor: identificacion de tipo de mensaje al mostrarse

--- src/components/toast/Toast.jsx
+++ src/components/toast/Toast.jsx
@@ -1,7 +1,12 @@
 export default function Toast({ id, type, message, onClose }) {
+  const icon = type === "success" ? "✓" : "✕";
+
   return (
     <div className={`toast ${type}`}>
+      <span className="toast-icon">{icon}</span>
+
       <span className="toast-message">{message}</span>
+
       <button
         type="button"
         className="toast-close-button"
