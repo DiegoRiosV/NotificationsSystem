6457c043573157ad08e5321a54b471810ef4bdf5
DiegoRiosV <diego.rios.v@ucb.edu.bo>
2026-07-10 12:14:13 -0400

    Paso 5 Creando Hooks

---
+++ src/hooks/useToast.js
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import ToastContext from "../context/ToastContext";
+
+export function useToast() {
+  const context = useContext(ToastContext);
+
+  if (!context) {
+    throw new Error(
+      "useToast debe utilizarse dentro de ToastProvider"
+    );
+  }
+
+  return context;
+}
+export default useToast;
