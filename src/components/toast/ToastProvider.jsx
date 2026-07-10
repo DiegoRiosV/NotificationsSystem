f8fd84c037d0639c80bf4ae0d27b2cad21c0e5c0
DiegoRiosV <diego.rios.v@ucb.edu.bo>
2026-07-10 12:26:48 -0400

    Paso 8, cerrando flujo completo

--- src/components/toast/ToastProvider.jsx
+++ src/components/toast/ToastProvider.jsx
@@ -1,12 +1,13 @@
 import { useState, useRef, useCallback } from "react";
 import ToastContext from "../../context/ToastContext";
+import Toast from "./Toast";
 
 const DEFAULT_DURATION = 3000;
 
 function ToastProvider({ children }) {
   const [toast, setToast] = useState(null);
 
-  const timeoutRef = useRef(null);
+ const timeoutRef = useRef(null);
 
   const showToast = useCallback(({ type, message, duration = DEFAULT_DURATION }) => {
    if (timeoutRef.current) {
@@ -26,6 +27,13 @@ function ToastProvider({ children }) {
   return (
     <ToastContext.Provider value={value}>
       {children}
+
+      {toast && (
+        <Toast
+          type={toast.type}
+          message={toast.message}
+        />
+      )}
     </ToastContext.Provider>
   );
 }
